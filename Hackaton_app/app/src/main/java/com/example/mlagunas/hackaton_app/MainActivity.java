8f7c8c3928b358a50621f1f74189fc5570a9396c
mlagunas <mlagunas94@gmail.com>
2015-07-11 12:42:02 +0200

    Adapter added

--- Hackaton_app/app/src/main/java/com/example/mlagunas/hackaton_app/MainActivity.java
+++ Hackaton_app/app/src/main/java/com/example/mlagunas/hackaton_app/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.mlagunas.hackaton_app;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListFragment frgListado
+                =(ListFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.Frg_list);
     }
 
     @Override
