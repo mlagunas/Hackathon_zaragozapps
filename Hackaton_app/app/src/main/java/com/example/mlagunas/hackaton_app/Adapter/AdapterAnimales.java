b3fa892194ad0510d34bd9e596d62ee42306c330
mlagunas <mlagunas94@gmail.com>
2015-07-12 00:55:42 +0200

    Order by <textbox>

--- Hackaton_app/app/src/main/java/com/example/mlagunas/hackaton_app/Adapter/AdapterAnimales.java
+++ Hackaton_app/app/src/main/java/com/example/mlagunas/hackaton_app/Adapter/AdapterAnimales.java
@@ -1,18 +1,19 @@
 package com.example.mlagunas.hackaton_app.Adapter;
 
 import android.app.Activity;
+import android.provider.SyncStateContract;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
 import com.example.mlagunas.hackaton_app.Objects.Animal;
 import com.example.mlagunas.hackaton_app.R;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,37 +23,98 @@ import java.util.List;
 public class AdapterAnimales extends ArrayAdapter<Animal> {
 
     private Activity context;
-    private ArrayList<Animal> data;
+    private ArrayList<Animal> data,dataInit;
 
-    public AdapterAnimales(Fragment context, ArrayList<Animal> data) {
+    public AdapterAnimales(Fragment context, ArrayList<Animal> d) {
 
-        super(context.getActivity(), R.layout.list_animales, data);
-        this.data = data;
+        super(context.getActivity(), R.layout.list_animales, d);
+        this.data = d;
+        dataInit = d;
         this.context = context.getActivity();
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View item = inflater.inflate(R.layout.list_animales, null);
 
-        TextView lblDe = (TextView) item.findViewById(R.id.lblnombre);
-        lblDe.setText(data.get(position).getNombre());
+        if(data!=null && position<data.size()) {
 
-        TextView lblAsunto = (TextView) item.findViewById(R.id.lblraza);
-        lblAsunto.setText(data.get(position).getRaza());
+            LayoutInflater inflater = context.getLayoutInflater();
+            View item = inflater.inflate(R.layout.list_animales, null);
 
-        return (item);
+            TextView lblDe = (TextView) item.findViewById(R.id.lblnombre);
+            lblDe.setText(data.get(position).getNombre());
+
+            TextView lblAsunto = (TextView) item.findViewById(R.id.lblraza);
+            lblAsunto.setText(data.get(position).getRaza());
+
+            return (item);
+        }
+
+       return convertView;
+    }
+
+    Filter myFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults filterResults = new FilterResults();
+            ArrayList<Animal> tempList=new ArrayList<Animal>();
+            //constraint is the result from text you want to filter against.
+            //objects is your data set you will filter from
+            if(constraint != null && dataInit!=null) {
+                int length=dataInit.size();
+                int i=0;
+                while(i<length){
+                    Animal item=dataInit.get(i);
+                    //do whatever you wanna do here
+                    //adding result set output array
+
+                    if (item.getRaza().toLowerCase().startsWith((constraint.toString()).toLowerCase()))
+                        tempList.add(item);
+                    i++;
+                }
+                //following two lines is very important
+                //as publish result can only take FilterResults objects
+                filterResults.values = tempList;
+                filterResults.count = tempList.size();
+            }
+            return filterResults;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void publishResults(CharSequence contraint, FilterResults results) {
+            data = (ArrayList<Animal>) results.values;
+            if (results.count > 0) {
+                notifyDataSetChanged();
+            } else {
+                notifyDataSetInvalidated();
+            }
+        }
+    };
+
+    @Override
+    public Filter getFilter() {
+        return myFilter;
+    }
+
+    public ArrayList<Animal> getFilteredResults(CharSequence constraint){
+        ArrayList<Animal> result = new ArrayList<Animal>();
+        if (constraint == null){
+            return dataInit;
+        }
+        else{
+            data = new ArrayList<Animal>();
+            for(Animal a: dataInit) {
+                    if (a.getEspecie().toLowerCase().startsWith(constraint.toString().toLowerCase())) {
+                        data.add(a);
+                    }
+            }
+            return data;
+        }
     }
 
     public void setData(ArrayList<Animal> d){
         this.data = d;
         super.
         notifyDataSetChanged();
-        Log.d("LONGITUDadapter", data.size() + " ");
-        Log.d("COLORs", data.get(10).getColor());
-
-
-
-
     }
 }
