ff9265c8bc8bfe882f956125fa32164a25f4eb8d
planaspa <planaspa@gmail.com>
2015-07-11 11:50:45 +0200

    Data getter class creation.

---
+++ Hackaton_app/app/src/main/java/com/example/mlagunas/hackaton_app/DataGetter.java
@@ -0,0 +1,15 @@
+package com.example.mlagunas.hackaton_app;
+
+/**
+ * Created by planaspa on 7/11/15.
+ */
+public class DataGetter {
+
+
+    private String URL;
+
+    public DataGetter (String URL){
+        this.URL = URL;
+        
+    }
+}
