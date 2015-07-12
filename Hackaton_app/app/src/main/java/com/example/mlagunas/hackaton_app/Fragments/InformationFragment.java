79cc55ab292561b2e127e789ffaf8bf08bac1228
planaspa <planaspa@gmail.com>
2015-07-12 03:15:00 +0200

    Contact button added.

--- Hackaton_app/app/src/main/java/com/example/mlagunas/hackaton_app/Fragments/InformationFragment.java
+++ Hackaton_app/app/src/main/java/com/example/mlagunas/hackaton_app/Fragments/InformationFragment.java
@@ -1,7 +1,9 @@
 package com.example.mlagunas.hackaton_app.Fragments;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -59,5 +64,8 @@ public class InformationFragment extends Fragment {
 
         TextView txtRaza = (TextView)getView().findViewById(R.id.textRaza);
         txtRaza.setText(raza);
+
+        Button btnSend = (Button) getView().findViewById(R.id.btnSend);
+
     }
 }
